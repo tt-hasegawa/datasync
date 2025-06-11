76aa4223252815abd244e3206160bae0fc0c6a54
tt-hasegawa <tt-hasegawa@matsusaka.co.jp>
2025-06-11 09:36:08 +0900

    単体テスト追加

--- Tools/jar_download.bat
+++ Tools/jar_download.bat
@@ -13,6 +13,7 @@ com/oracle/database/jdbc/ojdbc8/23.6.0.24.10/ojdbc8-23.6.0.24.10.jar
 com/microsoft/sqlserver/mssql-jdbc/12.10.0.jre8/mssql-jdbc-12.10.0.jre8.jar
 org/codehaus/groovy/groovy-all/2.4.21/groovy-all-2.4.21.jar 
 org/postgresql/postgresql/42.7.5/postgresql-42.7.5.jar 
+com/h2database/h2/2.1.214/h2-2.1.214.jar
 ) do (
     set "JAR_PATH=%%f"
     set "FILENAME=%%~nxf"
